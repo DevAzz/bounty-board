217a43fe44d8c8a61495d1ffb2682cf0ea1a9b42
slyashko <slyashko@i-novus.ru>
2024-10-16 22:07:20 +0300

    Задания по главе 1

---
+++ src/main/kotlin/Main_chapter1.kt
@@ -0,0 +1,19 @@
+package org.example
+
+const val HERO_NAME = "Madrigal"
+
+fun main() {
+    println("The hero announces her presence to the world.")
+    println(HERO_NAME)
+    var playerLevel = 4
+    println(playerLevel)
+
+    println("The hero embarks on her journey to locate the enchanted sword.")
+    playerLevel += 1
+    println(playerLevel)
+
+    val hasSteed = false
+    println(hasSteed)
+
+    println(HERO_NAME.reversed())
+}
